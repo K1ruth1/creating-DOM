cff82a2d3efdc9164b8b8c8f345ad65bcb23bded
K1ruth1 <135802282+K1ruth1@users.noreply.github.com>
2023-07-09 23:54:02 +0300

    Add files via upload

---
+++ phase-0-the-dom-modifying-elements-lab/index.js
@@ -0,0 +1,15 @@
+// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+// Create a new header element with an id of 'victory'
+const newHeader = document.createElement('h1#victory');
+newHeader.id = 'victory';
+
+// Set the text content of the new header element
+const yourName = 'Daniel'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+// Add the new header element to the document
+const body = document.querySelector('body');
+body.appendChild(newHeader);
